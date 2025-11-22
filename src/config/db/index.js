e45463ce287988444100442a4acc94bbbf9a427e
Quan Nguyen <157398135+Oplasama@users.noreply.github.com>
2025-11-22 20:09:40 +0200

    change mongodb, gitignore

--- src/config/db/index.js
+++ src/config/db/index.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 
 async function connect(){
     try{
-        const db_uri = process.env.MONGO_URL || 'mongodb://127.0.0.1:27017/baokim_dev'
+        const db_uri = process.env.MONGO_URI || 'mongodb://127.0.0.1:27017/baokim_dev'
         console.log('Connecting to MongoDB at', db_uri);
         await mongoose.connect(db_uri);
         console.log('Database connected successfully');
